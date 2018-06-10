b05628d518be0e5793f1a09796514819ddb57430
Tahfimul <victory.josh.josh24@gmail.com>
2018-06-10 18:49:37 -0400

    Updated script to format datasheets

--- formatData.py
+++ formatData.py
@@ -4,9 +4,10 @@ import codecs
 with codecs.open('cars.csv', encoding='utf-8') as rf:
     with open('cars.csv', 'a') as wf:
         size = 1
-        f_contents = rf.read(size)
-        while len(f_contents) > 0:
-            print(f_contents)
-            new = wf.write(f_contents.replace(';',','))
-            print(new)
-            f_contents = rf.read(size)
+        f_contents = rf.read()
+        new = wf.write(f_contents.replace(';',','))
+        # while len(f_contents) > 0:
+        #     print(f_contents)
+        #     new = wf.write(f_contents.replace(';',','))
+        #     print(new)
+        #     f_contents = rf.read(size)
